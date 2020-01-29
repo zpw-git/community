cc07c1a4ac4f0821533ff05028e8ad3d23260692
zhangpengwei <z18435737780@163.com>
2020-01-29 16:06:19 +0800

    页面初始

--- src/main/java/com/zpw/community/controller/HelloController.java
+++ src/main/java/com/zpw/community/controller/HelloController.java
@@ -16,7 +16,7 @@ public class HelloController {
   public String hello(@RequestParam(name = "name") String name , Model model){
 
     model.addAttribute("name",name);
-    return "hello";
+    return "index";
   }
 
 }
