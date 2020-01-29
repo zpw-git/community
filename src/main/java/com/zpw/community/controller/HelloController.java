49be8cadecd83e33a1fd05ecf50c67ae12dfe3f6
zhangpengwei <z18435737780@163.com>
2020-01-29 12:50:55 +0800

    第第一次提交

---
+++ src/main/java/com/zpw/community/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.zpw.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author: zhangpengwei
+ * @create: 2020-01-2020/1/29-11:45
+ */
+@Controller
+public class HelloController {
+
+  @GetMapping("/hello")
+  public String hello(@RequestParam(name = "name") String name , Model model){
+
+    model.addAttribute("name",name);
+    return "hello";
+  }
+
+}
